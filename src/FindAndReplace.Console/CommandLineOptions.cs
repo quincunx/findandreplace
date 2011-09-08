958d8b3edf18f89b7b1b6f3aec8e45e7be6f3a4c
Sasha Tsygankov <sasha@entechsolutions.com>
2011-09-09 01:33:39 +0600

    case 613: Implement command line

---
+++ src/FindAndReplace.Console/CommandLineOptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace FindAndReplace.Console
+{
+	public class CommandLineOptions : CommandLineOptionsBase
+	{
+		#region Standard Option Attribute
+		[Option(null, "find", Required = true, HelpText = "Text to find.")]
+		public string FindText = String.Empty;
+
+		[Option(null, "replace", HelpText = "Replacement text.")]
+		public string ReplaceText = String.Empty;
+
+		[Option(null, "dir", Required = true, HelpText = "Directory.")]
+		public string Dir = String.Empty;
+
+		[Option(null, "fileMask", Required = true,HelpText = "File mask.")]
+		public string FileMask = String.Empty;
+
+		#endregion
+
+		#region Specialized Option Attribute
+
+		[HelpOption("h", "help", HelpText = "Dispaly this help screen.")]
+		public string GetUsage()
+		{
+			var help = new HelpText("Find And Replace");
+			help.AdditionalNewLineAfterOption = true;
+			help.Copyright = new CopyrightInfo("Entech Solutions", 2011);
+			help.AddPreOptionsLine("This is free software. You may redistribute copies of it under the terms of");
+			help.AddPreOptionsLine("the MIT License <http://www.opensource.org/licenses/mit-license.php>.");
+			help.AddPreOptionsLine("Usage: \n\nFindAndReplace.Console.exe --find \"Text To Find\" --replace \"Text To Replace\"  --dir \"Directory Path\" --fileMask \"*.*\"\n");
+			help.AddPreOptionsLine("FindAndReplace.Console.exe --find \"Text To Find\" --dir \"Directory Path\" --fileMask \"*.*\"");
+			help.AddOptions(this);
+
+			return help;
+		}
+
+		#endregion
+	}
+}
---
+++ src/FindAndReplace.Tests/ReplacerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FindAndReplace.Tests
+{
+	[TestFixture]
+	public class ReplacerTest
+	{
+		private string _tempDir
+		{
+			get { return Path.GetTempPath() + "\\FindAndReplaceTests"; }
+		}
+	}
+}
