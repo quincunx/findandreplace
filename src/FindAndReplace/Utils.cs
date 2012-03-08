7c1a6a033c1b210a053dcb4b98523a244d070656
Eric Popivker <eric@entechsolutions.com>
2012-03-08 22:20:38 +0100

    Cleaned up some tests

---
+++ src/FindAndReplace/Utils.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace FindAndReplace
+{
+	internal static class Utils
+	{
+		public static RegexOptions GetRegExOptions(bool isCaseSensitive)
+		{
+			//Create a new option
+			var options = new RegexOptions();
+
+			//Is the match case check box checked
+			if (!isCaseSensitive)
+				options |= RegexOptions.IgnoreCase;
+
+			//Return the options
+			return options;
+		}
+
+		public static string[] GetFilesInDirectory(string dir, string fileMask, bool includeSubDirectories)
+		{
+			SearchOption searchOption = includeSubDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+			string[] filesInDirectory = Directory.GetFiles(dir, fileMask, searchOption);
+			return filesInDirectory;
+		}
+
+		public static bool IsBinaryFile(string fileContent)
+		{
+			//http://stackoverflow.com/questions/910873/how-can-i-determine-if-a-file-is-binary-or-text-in-c
+			if (fileContent.Contains("\0\0\0\0"))
+				return true;
+
+			return false;
+		}
+
+		public static List<MatchPreviewLineNumber> GetLineNumbersForMatchesPreview(string filePath, MatchCollection matches)
+		{
+			string content = string.Empty;
+
+			using (var sr = new StreamReader(filePath))
+			{
+				content = sr.ReadToEnd();
+			}
+
+			var separator = Environment.NewLine;
+			var lines = content.Split(new string[] { separator }, StringSplitOptions.None);
+
+			var result = new List<MatchPreviewLineNumber>();
+			var temp = new List<MatchPreviewLineNumber>();
+
+			foreach (Match match in matches)
+			{
+				var lineIndexStart = DetectMatchLine(lines.ToArray(), match.Index);
+				var lineIndexEnd = DetectMatchLine(lines.ToArray(), match.Index + match.Length);
+
+				for (int i = lineIndexStart - 2; i <= lineIndexEnd + 2; i++)
+				{
+					if (i >= 0 && i < lines.Count())
+					{
+						var lineNumber = new MatchPreviewLineNumber();
+						lineNumber.LineNumber = i;
+						lineNumber.HasMatch = (i >= lineIndexStart && i <= lineIndexEnd) ? true : false;
+						temp.Add(lineNumber);
+					}
+				}
+			}
+
+			result.AddRange(temp.Where(ln => ln.HasMatch).Distinct(new LineNumberComparer()));
+
+			result.AddRange(temp.Where(ln => !ln.HasMatch && !result.Select(l => l.LineNumber).Contains(ln.LineNumber)).Distinct(new LineNumberComparer()));
+
+			return result.OrderBy(ln => ln.LineNumber).ToList();
+		}
+
+		private static int DetectMatchLine(string[] lines, int position)
+		{
+			var separatorLength = 2;
+			int i = 0;
+			int charsCount = lines[0].Length + separatorLength;
+
+			while (charsCount <= position)
+			{
+				i++;
+				charsCount += lines[i].Length + separatorLength;
+			}
+
+			return i;
+		}
+	}
+}
