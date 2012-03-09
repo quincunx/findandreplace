773cd3becd983ed4bbb8eb4b572591d1411a6b9d
Eric Popivker <eric@entechsolutions.com>
2012-03-09 13:35:32 +0100

    Some UI cleanup for Remaining time

---
+++ src/FindAndReplace.Tests/ReplacerTest.cs
@@ -0,0 +1,231 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace FindAndReplace.Tests
+{
+	[TestFixture]
+	public class ReplacerTest : TestBase
+	{
+		[Test]
+		public void Replace_WhenSearchTextIsLicenseNoRegExpr_ReplacesTextInOne()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "license";
+			replacer.ReplaceText = "agreement";
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Cant find test files");
+
+			Assert.AreEqual(1, resultItems.Count);
+			Assert.AreEqual("test1.test", resultItems[0].FileName);
+			Assert.AreEqual(3, resultItems[0].NumMatches);
+			Assert.IsTrue(resultItems[0].IsSuccess);
+
+			resultItems = replacer.Replace().ResultItems;
+
+			resultItems = resultItems.Where(ri => ri.NumMatches != 0).ToList();
+			Assert.AreEqual(0, resultItems.Count);
+		}
+		
+		[Test]
+		public void Replace_WhenSearchTextIsEENoRegExpr_ReplacesTextInBoth()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "ee";
+			replacer.ReplaceText = "!!";
+
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			var matchedResultItems = resultItems.Where(ri => ri.NumMatches != 0).ToList();
+			Assert.AreEqual(2, matchedResultItems.Count);
+
+			var firstFile = resultItems.Where(ri => ri.FileName == "test1.test").ToList();
+
+			Assert.AreEqual(1, firstFile.Count);
+			Assert.AreEqual(5, firstFile[0].NumMatches);
+			Assert.IsTrue(firstFile[0].IsSuccess);
+
+			var secondFile = resultItems.Where(ri => ri.FileName == "test2.test").ToList();
+
+			Assert.AreEqual(1, secondFile.Count);
+			Assert.AreEqual(1, secondFile[0].NumMatches);
+			Assert.IsTrue(secondFile[0].IsSuccess);
+		}
+		
+		[Test]
+		public void Replace_WhenSearchTextIsNewYorkNoRegExpr_NoReplacesText()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "New York";
+			replacer.ReplaceText = "Moscow";
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			Assert.AreEqual(0, resultItems.Count);
+		}
+		
+		[Test]
+		public void Replace_WhenSearchMaskIsTxtOnlyNoRegExpr_NoRepacesText()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.txt";
+			replacer.FindText = "a";
+			replacer.ReplaceText = "b";
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			Assert.AreEqual(0, resultItems.Count);
+		}
+
+		[Test]
+		public void Replace_WhenSearchMaskIsTest1NoRegExpr_ReplacesTextInOne()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "test1.*";
+			replacer.FindText = "ee";
+			replacer.ReplaceText = "!!";
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			var matchedResultItems = resultItems.Where(ri => ri.NumMatches != 0).ToList();
+
+			Assert.AreEqual(1, matchedResultItems.Count);
+			Assert.AreEqual(5, matchedResultItems[0].NumMatches);
+			Assert.AreEqual("test1.test", matchedResultItems[0].FileName);
+			Assert.IsTrue(matchedResultItems[0].IsSuccess);
+
+			resultItems = replacer.Replace().ResultItems;
+
+			matchedResultItems = resultItems.Where(ri => ri.NumMatches != 0).ToList();
+
+			Assert.AreEqual(0, matchedResultItems.Count);
+		}
+
+		[Test]
+		public void Replace_WhenSearchTextIsSoAndCaseSensitiveNoRegExpr_ReplacesTextInOne()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "So";
+			replacer.IsCaseSensitive = true;
+			replacer.ReplaceText = "Su";
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			Assert.AreEqual(1, resultItems.Count);
+			Assert.AreEqual("test1.test", resultItems[0].FileName);
+			Assert.AreEqual(1, resultItems[0].NumMatches);
+			Assert.IsTrue(resultItems[0].IsSuccess);
+
+		
+			resultItems = replacer.Replace().ResultItems;
+			Assert.AreEqual(0, resultItems.Count);
+		}
+		
+		[Test]
+		public void Replace_WhenSearchTextIsEEAndUseSubDirNoRegExpr_ReplacesTextInFour()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "ee";
+			replacer.ReplaceText = "!!";
+			replacer.IncludeSubDirectories = true;
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			var matchedResultItems = resultItems.Where(ri => ri.NumMatches != 0).ToList();
+
+			Assert.AreEqual(4, matchedResultItems.Count);
+
+			var firstFile = resultItems.Where(ri => ri.FileName == "test1.test").ToList();
+
+			Assert.AreEqual(2, firstFile.Count);
+			Assert.AreEqual(5, firstFile[0].NumMatches);
+			Assert.AreEqual(5, firstFile[1].NumMatches);
+			Assert.IsTrue(firstFile[0].IsSuccess);
+			Assert.IsTrue(firstFile[1].IsSuccess);
+
+			var secondFile = resultItems.Where(ri => ri.FileName == "test2.test").ToList();
+
+			Assert.AreEqual(2, secondFile.Count);
+			Assert.AreEqual(1, secondFile[0].NumMatches);
+			Assert.IsTrue(secondFile[0].IsSuccess);
+			Assert.AreEqual(1, secondFile[1].NumMatches);
+			Assert.IsTrue(secondFile[1].IsSuccess);
+		}
+
+		[Test]
+		public void Replace_WhenSearchTextIsRegularExpression_ReplacesTextInOne()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = @"\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b"; //email pattern
+			replacer.ReplaceText = "Email was here..";
+			replacer.FindTextHasRegEx = true;
+
+			var resultItems = replacer.Replace().ResultItems;
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			Assert.AreEqual(1, resultItems.Count);
+			Assert.AreEqual("test2.test", resultItems[0].FileName);
+			Assert.AreEqual(1, resultItems[0].NumMatches);
+		}
+
+		[Test]
+		public void Replace_WhenFileIsReadonly_CanNotRepacesText()
+		{
+			Replacer replacer = new Replacer();
+
+			replacer.Dir = _tempDir;
+			replacer.FileMask = "*.*";
+			replacer.FindText = "readonly";
+			replacer.ReplaceText = "I can replace";
+
+			var resultItems = replacer.Replace().ResultItems.Where(r=>!r.IsSuccess).ToList();
+
+			if (resultItems == null || resultItems.Count == 0)
+				Assert.Fail("Can't find test files");
+
+			Assert.AreEqual(1, resultItems.Count);
+			Assert.AreEqual("test3.test", resultItems[0].FileName);
+			Assert.IsTrue(resultItems[0].FailedToWrite);
+			Assert.IsNotEmpty(resultItems[0].ErrorMessage);
+		}
+	}
+}
---
+++ src/FindAndReplace/Utils.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace FindAndReplace
+{
+	public static class Utils
+	{
+		public static RegexOptions GetRegExOptions(bool isCaseSensitive)
+		{
+			//Create a new option
+			var options = new RegexOptions();
+
+			//Is the match case check box checked
+			if (!isCaseSensitive)
+				options |= RegexOptions.IgnoreCase;
+
+			//Return the options
+			return options;
+		}
+
+		public static string[] GetFilesInDirectory(string dir, string fileMask, bool includeSubDirectories)
+		{
+			SearchOption searchOption = includeSubDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+			string[] filesInDirectory = Directory.GetFiles(dir, fileMask, searchOption);
+			return filesInDirectory;
+		}
+
+		public static bool IsBinaryFile(string fileContent)
+		{
+			//http://stackoverflow.com/questions/910873/how-can-i-determine-if-a-file-is-binary-or-text-in-c
+			if (fileContent.Contains("\0\0\0\0"))
+				return true;
+
+			return false;
+		}
+
+		public static List<MatchPreviewLineNumber> GetLineNumbersForMatchesPreview(string filePath, MatchCollection matches)
+		{
+			string content = string.Empty;
+
+			using (var sr = new StreamReader(filePath))
+			{
+				content = sr.ReadToEnd();
+			}
+
+			var separator = Environment.NewLine;
+			var lines = content.Split(new string[] { separator }, StringSplitOptions.None);
+
+			var result = new List<MatchPreviewLineNumber>();
+			var temp = new List<MatchPreviewLineNumber>();
+
+			foreach (Match match in matches)
+			{
+				var lineIndexStart = DetectMatchLine(lines.ToArray(), match.Index);
+				var lineIndexEnd = DetectMatchLine(lines.ToArray(), match.Index + match.Length);
+
+				for (int i = lineIndexStart - 2; i <= lineIndexEnd + 2; i++)
+				{
+					if (i >= 0 && i < lines.Count())
+					{
+						var lineNumber = new MatchPreviewLineNumber();
+						lineNumber.LineNumber = i;
+						lineNumber.HasMatch = (i >= lineIndexStart && i <= lineIndexEnd) ? true : false;
+						temp.Add(lineNumber);
+					}
+				}
+			}
+
+			result.AddRange(temp.Where(ln => ln.HasMatch).Distinct(new LineNumberComparer()));
+
+			result.AddRange(temp.Where(ln => !ln.HasMatch && !result.Select(l => l.LineNumber).Contains(ln.LineNumber)).Distinct(new LineNumberComparer()));
+
+			return result.OrderBy(ln => ln.LineNumber).ToList();
+		}
+
+		public static string FormatTimeSpan(TimeSpan timeSpan)
+		{
+			string result = String.Empty;
+
+			int h = timeSpan.Hours;
+			int m = timeSpan.Minutes;
+			int s = timeSpan.Seconds;
+
+			if (h > 0)
+			{
+				result += String.Format("{0}h ", h);
+
+				if (m > 0)
+				{
+					result += String.Format("{0}m ", m);
+
+					if (s > 0) result += String.Format("{0}s ", s);
+				}
+				else
+				{
+					if (s > 0)
+					{
+						result += String.Format("{0}m ", m);
+
+						result += String.Format("{0}s ", s);
+					}
+				}
+
+			}
+			else
+			{
+				if (m > 0) result += String.Format("{0}m ", m);
+
+				if (s > 0) result += String.Format("{0}s ", s);
+			}
+
+			return result;
+		}
+
+		private static int DetectMatchLine(string[] lines, int position)
+		{
+			var separatorLength = 2;
+			int i = 0;
+			int charsCount = lines[0].Length + separatorLength;
+
+			while (charsCount <= position)
+			{
+				i++;
+				charsCount += lines[i].Length + separatorLength;
+			}
+
+			return i;
+		}
+	}
+}
