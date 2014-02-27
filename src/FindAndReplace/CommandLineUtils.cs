6c7d138cd9e0bf53712f820e52a84147c8b6cd8e
Eric Popivker <eric@entechsolutions.com>
2014-02-27 14:28:37 +0100

    Moved some things around in form

---
+++ src/FindAndReplace/CommandLineUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindAndReplace
+{
+	public static class CommandLineUtils
+	{
+		public static string EncodeText(string original, bool isRegularExpression = false)
+		{
+			var result = original.Replace(Environment.NewLine, "\\n");
+			
+			if (!isRegularExpression)
+				result = result.Replace(@"\", @"\\");
+
+			return result;
+		}
+
+		public static string DecodeText(string original, bool hasRegEx = false)
+		{
+		    string decoded = original;
+
+            //For RegEx there is no new lines, so we don't want to replace \\n
+            //See case https://findandreplace.codeplex.com/workitem/17
+		    if (!hasRegEx) 
+		        decoded = decoded.Replace("\\n", Environment.NewLine);  
+			
+			decoded = decoded.Replace(@"\\", @"\");
+			return decoded;
+		}
+	}
+}
+
+
