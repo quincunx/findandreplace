4e0ef1eac18da6b0d7c627ce79dcc406496906ab
Sasha Tsygankov <sasha@entechsolutions.com>
2014-03-08 03:24:39 +0600

    fix IsValidEscapeSequence

---
+++ src/FindAndReplace/CommandLineUtils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FindAndReplace
+{
+	public static class CommandLineUtils
+	{
+		public static string EncodeText(string original, bool isRegularExpression = false, bool useEscapeChars = false)
+		{
+			var result = original.Replace(Environment.NewLine, "\\n");
+			
+			if (!isRegularExpression && !useEscapeChars)
+				result = result.Replace(@"\", @"\\");
+
+			return result;
+		}
+
+		public static string DecodeText(string original, bool hasRegEx = false, bool useEscapeChars = false)
+		{
+		    string decoded = original;
+
+            //For RegEx there is no new lines, so we don't want to replace \\n
+            //See case https://findandreplace.codeplex.com/workitem/17
+		    if (!hasRegEx && !useEscapeChars) 
+		        decoded = decoded.Replace("\\n", Environment.NewLine);  
+			
+			decoded = decoded.Replace(@"\\", @"\");
+			return decoded;
+		}
+
+		public static string FormatArg(string original, bool isRegularExpression = false, bool useEscapeChars = false)
+		{
+			var argValue = EncodeText(original, isRegularExpression, useEscapeChars);
+			
+			//windows arg can't end with odd count of '\'
+			//args can ends with even count of '\'
+			//if arg ends with odd count of '\' we add one more to the end
+			var regexPattern = @"\\+$";
+
+			var regex = new Regex(regexPattern);
+
+			var matches = regex.Matches(argValue);
+
+			if (matches.Count > 0)
+			{
+				var match = matches[0];
+
+				if ((match.Length%2) != 0)
+					argValue += @"\";
+			}
+
+			return argValue;
+		}
+	}
+}
+
+
