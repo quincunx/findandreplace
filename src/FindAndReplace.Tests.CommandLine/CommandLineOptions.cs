1fd12f6029326571705630fc5500ab8c07907ad8
Sasha Tsygankov <sasha@entechsolutions.com>
2014-03-11 00:32:10 +0600

    Add FindAndReplace.Tests.CommandLine

---
+++ src/FindAndReplace.Tests.CommandLine/CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace FindAndReplace.Tests.CommandLine
+{
+	public class CommandLineOptions
+	{
+		#region Standard Option Attribute
+
+		[Option("testVal", Required = true, HelpText = "Test value to parse.")]
+		public string TestValue { get; set; }
+
+		#endregion
+	}
+}
---
+++ src/FindAndReplace.Tests.CommandLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace FindAndReplace.Tests.CommandLine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var options = new CommandLineOptions();
+
+			var result = "not decoded";
+			if (Parser.Default.ParseArguments(args, options))
+			{
+				result = options.TestValue;
+			}
+
+			using (var outfile = new StreamWriter("output.log"))
+			{
+				outfile.WriteLine(result);
+			} 
+		}
+	}
+}
