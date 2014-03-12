0d02f26d6255338a2acf4cb40fd4e82a0afe5dd5
Eric Popivker <eric@entechsolutions.com>
2014-03-12 15:48:22 +0100

    Some minor cleanup

---
+++ src/FindAndReplace.Tests/CommandLineUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace FindAndReplace.Tests
+{
+	[TestFixture]
+	public class CommandLineUtilsTest
+	{
+
+		//From https://findandreplace.codeplex.com/workitem/17
+		[Test]
+		public void Encode_Decode_FromWorkItem17_ReturnsSameValue()
+		{
+			string original = "\\r(?!\\n)";
+			string encoded = CommandLineUtils.EncodeText(original);
+			string decoded = CommandLineUtils.DecodeText(encoded, false, true);
+
+			Assert.AreEqual(original, decoded);
+		}
+
+
+	}
+}
